944d5bc089d08edc891f9f14769b57de73dce6b9
Ryan Daniels <ryan.aka.spike@gmail.com>
2022-12-03 23:15:28 +0200

    WIP: setting up MySQL in Node

--- server.js
+++ server.js
@@ -1,8 +1,32 @@
+const mysql = require('mysql');
 const express = require('express');
 const app = express();
 
 const HOST = 'localhost';
 const PORT = 3000;
+
+const connection = mysql.createConnection({
+    host: HOST,
+    user: 'azura',
+    password: 'YouShouldHireRyan',
+    database: 'Azura'
+});
+
+connection.connect(error => {
+    if (error) {
+        console.log('DB connection error', error.stack);
+        return;
+    }
+
+    console.log(`Connected as id: ${connection.threadId}`);
+})
+
+app.use((req, res, next) => {
+    const startTime = Date.now();
+    next();
+    const delta = Date.now() - startTime;
+    console.log(`request logged for ${req.method}: ${req.url} on ${new Date()} in ${delta}ms`);
+});
 
 app.get('/', (req, res) => {
     res.json({ message: 'Hello World!' });

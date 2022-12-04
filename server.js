dc09673bd2860b1daf0c1f7b098f979055d44524
Ryan Daniels <ryan.aka.spike@gmail.com>
2022-12-04 23:40:19 +0200

    Installed and set up ESLint for the project
    After configuring the ESLint setup, I corrected and formatted the code based on ESLint errors and warnings

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
+/* eslint-disable no-console */
 const path = require('path');
 const mysql = require('mysql');
 const express = require('express');
+
 const app = express();
 
 const HOST = 'localhost';
 const PORT = 3000;
 
 const connection = mysql.createConnection({
-    host: HOST,
-    user: 'ryan',
-    password: 'youshouldhireme',
-    database: 'Azura'
+  host: HOST,
+  user: 'ryan',
+  password: 'youshouldhireme',
+  database: 'Azura',
 });
 
-connection.connect(error => {
-    if (error) {
-        console.log('DB connection error', error.stack);
-        return;
-    }
+connection.connect((error) => {
+  if (error) {
+    console.log('DB connection error', error.stack);
+    return;
+  }
 
-    console.log(`Connected as id: ${connection.threadId}`);
+  console.log(`Connected as id: ${connection.threadId}`);
 });
 
 app.set('view engine', 'hbs');
@@ -27,10 +29,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.use('/site', express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    const startTime = Date.now();
-    next();
-    const delta = Date.now() - startTime;
-    console.log(`request logged from IP: ${req.ip} for ${req.method}: ${req.url} on ${new Date()} in ${delta}ms`);
+  const startTime = Date.now();
+  next();
+  const delta = Date.now() - startTime;
+  console.log(`request logged from IP: ${req.ip} for ${req.method}: ${req.url} on ${new Date()} in ${delta}ms`);
 });
 
 app.use(express.json());
@@ -43,64 +45,80 @@ app.use(express.json());
 // });
 
 app.get('/health', (req, res) => {
-    res.json({ message: 'Server is healthy!' });
+  res.json({
+    message: 'Server is healthy!',
+  });
 });
 
 app.get(['/', '/vehicles'], (req, res) => {
-    let vehicles = [];
-    connection.query('SELECT * FROM Vehicle', function (error, results, fields) {
-        if (error) {
-            console.log('Error getting data', error);
-            return;
-        }
+  const vehicles = [];
+  connection.query('SELECT * FROM Vehicle', (error, results) => {
+    if (error) {
+      console.log('Error getting data', error);
+      return;
+    }
 
-        vehicles.push(...results);
-    });
+    vehicles.push(...results);
+  });
 
-    res.render('index', {
-        title: 'Azura Assignment',
-        pageHeader: 'Welcome to Ryan\'s assignment for Azura Media Limited',
-        vehicles
-    });
+  res.render('index', {
+    title: 'Azura Assignment',
+    pageHeader: 'Welcome to Ryan\'s assignment for Azura Media Limited',
+    vehicles,
+  });
 });
 
 app.get('/vehicles/:id', (req, res) => {
-    const id = Number(req.params.id);
-    let vehicle;
-    connection.query('SELECT * FROM Vehicle WHERE Id = ?', [id], function (error, results, fields) {
-        if (error) {
-            console.log('Error getting data', error);
-            return;
-        }
-        
-        if (results.length > 0) {
-            vehicle = results[0];
-        }
+  const id = Number(req.params.id);
+  let vehicle;
+  connection.query(
+    'SELECT * FROM Vehicle WHERE Id = ?',
+    [id],
+    (error, results) => {
+      if (error) {
+        console.log('Error getting data', error);
+        return;
+      }
 
-        res.render('view-vehicle', {
-            title: 'Azura Assignment',
-            pageHeader: 'Viewing vehicle details',
-            vehicle
-        });
-    })
+      if (results.length > 0) {
+        [vehicle] = results;
+      }
+
+      res.render('view-vehicle', {
+        title: 'Azura Assignment',
+        pageHeader: 'Viewing vehicle details',
+        vehicle,
+      });
+    },
+  );
 });
 
 app.post('/vehicles', (req, res) => {
-    const { make, model, mileage, colour, location, value } = req.body;
-    connection.query('INSERT INTO Vehicle (Make, Model, Mileage, Colour, Location, `Value`) VALUES(?, ?, ?, ?, ?, ?)',
-                    [make, model, mileage, colour, location, value], function (error, results, fields) {
-        if (error) {
-            console.log('Error getting data', error);
-        }
+  const {
+    make, model, mileage, colour, location, value,
+  } = req.body;
+  connection.query(
+    'INSERT INTO Vehicle (Make, Model, Mileage, Colour, Location, `Value`) VALUES(?, ?, ?, ?, ?, ?)',
+    [make, model, mileage, colour, location, value],
+    (error, results) => {
+      if (error) {
+        console.log('Error getting data', error);
+      }
 
-        res.status(201).json({ id: results.insertId, message: 'Vehicle captured successfully' });
-    })
+      res.status(201).json({
+        id: results.insertId,
+        message: 'Vehicle captured successfully',
+      });
+    },
+  );
 });
 
 app.get('*', (req, res) => {
-    res.status(404).json({ message: 'Not Found' });
+  res.status(404).json({
+    message: 'Not Found',
+  });
 });
 
 app.listen(PORT, HOST, () => {
-    console.log(`Server running at http://${HOST}:${PORT}/`);
+  console.log(`Server running at http://${HOST}:${PORT}/`);
 });
